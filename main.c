bec3942111d99d32d41141493cb9623bed12abae
ADiesner <mcskan@hotmail.de>
2019-12-04 10:16:21 +0100

    Version 2 Kommentare

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct
 }bmpheader;
 
 #pragma pack(2)
-typedef struct
+typedef struct // Bitmapinfoheader
 {
     unsigned int biSize;
     unsigned int biWidth;
@@ -26,14 +26,14 @@ typedef struct
     unsigned int biClrImportant;
 }bmpinfo;
 
-typedef struct
+typedef struct // Farbtabelle
 {
     unsigned char cB;
     unsigned char cG;
     unsigned char cR;
 } bmpcolor;
 
-void printheader(bmpheader *tbmpfh)
+void printheader(bmpheader *tbmpfh) // Bitmapheader auf die Konsole ausprinten
 {
     printf("Type: %d \n", tbmpfh->bfType);
     printf("Size:%d \n", tbmpfh->bfSize);
@@ -41,7 +41,7 @@ void printheader(bmpheader *tbmpfh)
     printf("OffBits: %d \n", tbmpfh->bfOffBits);
 }
 
-void printinfo(bmpinfo *tbmpif)
+void printinfo(bmpinfo *tbmpif) // Bitmapinfoheader auf die Konsole ausprinten
 {
     printf("Size: %d \n", tbmpif->biSize);
     printf("Width: %d \n", tbmpif->biWidth);
@@ -65,20 +65,20 @@ int main(int argc, char *argv[])
     bmpinfo tbmpif;
     bmpcolor **tcolortab;
 
-    FILE *fBmpdatei = fopen(argv[1],"rb");
-    FILE *fuellerneu = fopen("fuellerneu.bmp","wb");
+    FILE *fBmpdatei = fopen(argv[1],"rb"); //Datei öffnen
+    FILE *fuellerneu = fopen("fuellerneu.bmp","wb"); // neue Datei erstellen
 
-    fread(&tbmpfh,sizeof(bmpheader),1,fBmpdatei);
-    fread(&tbmpif,sizeof(bmpinfo),1,fBmpdatei);
+    fread(&tbmpfh,sizeof(bmpheader),1,fBmpdatei); // Bitmapheader einlesen in die Struktur fBmpdatei
+    fread(&tbmpif,sizeof(bmpinfo),1,fBmpdatei); // Bitmapinfoheader einlesen in die Struktur fBmpdatei
 
-    tcolortab = (bmpcolor**)malloc(tbmpif.biHeight*sizeof(bmpcolor*));
+    tcolortab = (bmpcolor**)malloc(tbmpif.biHeight*sizeof(bmpcolor*)); // Speicher reservieren für die erste Dimension
 
-    for(iL=0; iL<tbmpif.biHeight;iL++)
+    for(iL=0; iL<tbmpif.biHeight;iL++) // Speicher reservieren für die zweite Dimension
     {
         tcolortab[iL] = (bmpcolor*)malloc(tbmpif.biWidth*sizeof(bmpcolor));
     }
 
-    for(iX=0 ; iX<tbmpif.biHeight; iX++)
+    for(iX=0 ; iX<tbmpif.biHeight; iX++) // Farbtabelle einlesen
     {
         for(iY=0; iY<tbmpif.biWidth; iY++)
         {
@@ -86,8 +86,9 @@ int main(int argc, char *argv[])
         }
         fseek(fBmpdatei,tbmpif.biWidth%4,SEEK_CUR);
     }
-    fwrite(&tbmpfh,sizeof(bmpheader),1,fuellerneu);
-    fwrite(&tbmpif,sizeof(bmpinfo),1,fuellerneu);
+
+    fwrite(&tbmpfh,sizeof(bmpheader),1,fuellerneu); // Bitmapheader in die neue Datei reinschreiben
+    fwrite(&tbmpif,sizeof(bmpinfo),1,fuellerneu); // Bitmapinfoheader in die neue Datei reinschreiben
 
 
     for(iX=0 ; iX<tbmpif.biHeight; iX++)
@@ -97,25 +98,21 @@ int main(int argc, char *argv[])
             fwrite(&tcolortab[iX][iY],sizeof(bmpcolor),1,fuellerneu);
         }
 
-
         if(4-tbmpif.biWidth%4 != 0)
         {
             for(iL=0; iL<4-tbmpif.biWidth%4; iL++)
             {
                 fputc(0,fuellerneu);
             }
-
         }
-
-
     }
 
-    printheader(&tbmpfh);
+    printheader(&tbmpfh); //Bitmapheader auf der Konsole anzeigen lassen
     printf("\n");
-    printinfo(&tbmpif);
+    printinfo(&tbmpif); // Bitmapinfoheader auf der Konsole anzeigen lassen
 
 
-    fclose(fuellerneu);
-    fclose(argv[1]);
+    fclose(fuellerneu); // Neue Datei schließen
+    fclose(argv[1]); // Alte Datei schließen
     return 0;
 }
