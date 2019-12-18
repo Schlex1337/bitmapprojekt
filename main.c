7a07179614be23e8e6fbac89fe8e803ad5dbc273
ADiesner <mcskan@hotmail.de>
2019-12-18 10:05:53 +0100

    Version 4.2 Funktionauslagerung

--- main.c
+++ main.c
@@ -33,40 +33,52 @@ typedef struct // Farbtabelle
     unsigned char cR;
 } bmpcolor;
 
-void printheader(bmpheader *tbmpfh) // Bitmapheader auf die Konsole ausprinten
+void printheader(bmpheader *bhead) // Bitmapheader auf die Konsole ausprinten
 {
-    printf("Type: %d \n", tbmpfh->bfType);
-    printf("Size:%d \n", tbmpfh->bfSize);
-    printf("Reserved:%d \n", tbmpfh->bfReserverd);
-    printf("OffBits: %d \n", tbmpfh->bfOffBits);
+    printf("Type: %d \n", bhead->bfType);
+    printf("Size:%d \n", bhead->bfSize);
+    printf("Reserved:%d \n", bhead->bfReserverd);
+    printf("OffBits: %d \n", bhead->bfOffBits);
 }
 
-void printinfo(bmpinfo *tbmpif) // Bitmapinfoheader auf die Konsole ausprinten
+void printinfo(bmpinfo *binfo) // Bitmapinfoheader auf die Konsole ausprinten
 {
-    printf("Size: %d \n", tbmpif->biSize);
-    printf("Width: %d \n", tbmpif->biWidth);
-    printf("Height:%d \n", tbmpif->biHeight);
-    printf("Planes: %d \n", tbmpif->biPlanes);
-    printf("BitCount: %d \n", tbmpif->biBitCount);
-    printf("Compression: %d \n", tbmpif->biCompression);
-    printf("SizeImage: %d \n", tbmpif->biSizelmange);
-    printf("XPelsPerMeter: %d \n", tbmpif->biXPelsPerMeter);
-    printf("YPelsPerMeter: %d \n", tbmpif->biYPelsPerMeter);
-    printf("Clr Used:%d \n", tbmpif->biClrUsed);
-    printf("Clr IMPORTANT: %d \n", tbmpif->biClrImportant);
+    printf("Size: %d \n", binfo->biSize);
+    printf("Width: %d \n", binfo->biWidth);
+    printf("Height:%d \n", binfo->biHeight);
+    printf("Planes: %d \n", binfo->biPlanes);
+    printf("BitCount: %d \n", binfo->biBitCount);
+    printf("Compression: %d \n", binfo->biCompression);
+    printf("SizeImage: %d \n", binfo->biSizelmange);
+    printf("XPelsPerMeter: %d \n", binfo->biXPelsPerMeter);
+    printf("YPelsPerMeter: %d \n", binfo->biYPelsPerMeter);
+    printf("Clr Used:%d \n", binfo->biClrUsed);
+    printf("Clr IMPORTANT: %d \n", binfo->biClrImportant);
 }
+/*
+void speicher(bmpheader *bhead,bmpinfo *binfo, bmpcolor *tcolor)
+{
+    fread(&bhead,sizeof(bmpheader),1,fBmpdatei); // Bitmapheader einlesen in die Struktur fBmpdatei
+    fread(&binfo,sizeof(bmpinfo),1,fBmpdatei); // Bitmapinfoheader einlesen in die Struktur fBmpdatei
+
+    tcolortab = (bmpcolor**)malloc(binfo.biHeight*sizeof(bmpcolor*)); // Speicher reservieren für die erste Dimension
+    for(iL=0; iL<binfo.biHeight;iL++) // Speicher reservieren für die zweite Dimension
+    {
+        tcolortab[iL] = (bmpcolor*)malloc(binfo.biWidth*sizeof(bmpcolor));
+    }
+}*/
 
 
 
 int main(int argc, char *argv[])
 {
     int iX , iY, iL, iP;
-    bmpheader tbmpfh;
-    bmpinfo tbmpif;
+    bmpheader bhead;
+    bmpinfo binfo;
     bmpcolor **tcolortab;
 
-    FILE *fBmpdatei = fopen("fueller.bmp","rb");
-    FILE *neu = fopen("fuellerneu2.bmp","wb"); // neue Datei erstellen
+    FILE *fBmpdatei = fopen("naegel.bmp","rb");
+    FILE *neu = fopen("naegelneu.bmp","wb"); // neue Datei erstellen
 
     if(fBmpdatei == NULL) // Geöffnete Datei vorhanden?
     {
@@ -74,30 +86,31 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    fread(&tbmpfh,sizeof(bmpheader),1,fBmpdatei); // Bitmapheader einlesen in die Struktur fBmpdatei
-    fread(&tbmpif,sizeof(bmpinfo),1,fBmpdatei); // Bitmapinfoheader einlesen in die Struktur fBmpdatei
 
-    tcolortab = (bmpcolor**)malloc(tbmpif.biHeight*sizeof(bmpcolor*)); // Speicher reservieren für die erste Dimension
-    for(iL=0; iL<tbmpif.biHeight;iL++) // Speicher reservieren für die zweite Dimension
+    fread(&bhead,sizeof(bmpheader),1,fBmpdatei); // Bitmapheader einlesen in die Struktur fBmpdatei
+    fread(&binfo,sizeof(bmpinfo),1,fBmpdatei); // Bitmapinfoheader einlesen in die Struktur fBmpdatei
+
+    tcolortab = (bmpcolor**)malloc(binfo.biHeight*sizeof(bmpcolor*)); // Speicher reservieren für die erste Dimension
+    for(iL=0; iL<binfo.biHeight;iL++) // Speicher reservieren für die zweite Dimension
     {
-        tcolortab[iL] = (bmpcolor*)malloc(tbmpif.biWidth*sizeof(bmpcolor));
+        tcolortab[iL] = (bmpcolor*)malloc(binfo.biWidth*sizeof(bmpcolor));
     }
 
-    for(iX=0 ; iX<tbmpif.biHeight; iX++) // Farbtabelle einlesen
+    for(iX=0 ; iX<binfo.biHeight; iX++) // Farbtabelle einlesen
     {
-        for(iY=0; iY<tbmpif.biWidth; iY++)
+        for(iY=0; iY<binfo.biWidth; iY++)
         {
             fread(&tcolortab[iX][iY],sizeof(bmpcolor),1,fBmpdatei);
         }
-        fseek(fBmpdatei,tbmpif.biWidth%4,SEEK_CUR);
+        fseek(fBmpdatei,binfo.biWidth%4,SEEK_CUR);
     }
 
-    fwrite(&tbmpfh,sizeof(bmpheader),1,neu); // Bitmapheader in die neue Datei reinschreiben
-    fwrite(&tbmpif,sizeof(bmpinfo),1,neu); // Bitmapinfoheader in die neue Datei reinschreiben
+    fwrite(&bhead,sizeof(bmpheader),1,neu); // Bitmapheader in die neue Datei reinschreiben
+    fwrite(&binfo,sizeof(bmpinfo),1,neu); // Bitmapinfoheader in die neue Datei reinschreiben
 
-    for(iX=0; iX<tbmpif.biHeight; iX++) // Graustufe
+    for(iX=0; iX<binfo.biHeight; iX++) // Graustufe
     {
-       for(iY=0; iY<tbmpif.biWidth; iY++)
+       for(iY=0; iY<binfo.biWidth; iY++)
         {
             iP = tcolortab[iX][iY].cR*0.299+tcolortab[iX][iY].cG*0.587+tcolortab[iX][iY].cB*0.114;
             tcolortab[iX][iY].cB = iP;
@@ -106,25 +119,25 @@ int main(int argc, char *argv[])
         }
     }
 
-    for(iX=0; iX<tbmpif.biHeight; iX++) // Farbtabelle in die neue Datei reinschreiben
+    for(iX=0; iX<binfo.biHeight; iX++) // Farbtabelle in die neue Datei reinschreiben
     {
-       for(iY=0; iY<tbmpif.biWidth; iY++)
+       for(iY=0; iY<binfo.biWidth; iY++)
         {
             fwrite(&tcolortab[iX][iY],sizeof(bmpcolor),1,neu);
         }
 
-        if(tbmpif.biWidth%4 != 0)
+        if(binfo.biWidth%4 != 0)
         {
-            for(iL=0; iL<4-tbmpif.biWidth%4; iL++)
+            for(iL=0; iL<4-binfo.biWidth%4; iL++)
             {
                 fputc(0,neu);
             }
         }
     }
 
-    printheader(&tbmpfh); //Bitmapheader auf der Konsole anzeigen lassen
+    printheader(&bhead); //Bitmapheader auf der Konsole anzeigen lassen
     printf("\n");
-    printinfo(&tbmpif); // Bitmapinfoheader auf der Konsole anzeigen lassen
+    printinfo(&binfo); // Bitmapinfoheader auf der Konsole anzeigen lassen
 
     fclose(neu); // Neue Datei schließen
     fclose(fBmpdatei); // Alte Datei schließen
