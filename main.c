e9cce7275457205f5f685cb89758702ad3f99551
Schlex <mcskan@hotmail.de>
2020-01-22 19:06:49 +0100

    Graustufe und Drehen funktioniert

--- main.c
+++ main.c
@@ -111,23 +111,43 @@ void tabellealt(FILE *neu, int height, int width, bmpcolor **tcolortab, FILE *fB
         }
 }
 
-/*
-void speicher(int height, int width, bmpcolor ***tcolortab)
+void writeColortabT(FILE *neu, bmpheader *tbmpfh, bmpinfo *tbmpif, bmpcolor **tcolortab)
 {
+    int iX, iY, iL, iTemp;
 
-    tcolortab = (bmpcolor**)malloc(height*sizeof(bmpcolor*)); // Speicher reservieren für die erste Dimension
-    for(int iL=0; iL<height;iL++) // Speicher reservieren für die zweite Dimension
+    iTemp = tbmpif->biHeight;        //Höhe und Breite tauschen
+
+    tbmpif->biHeight= tbmpif->biWidth;
+    tbmpif->biWidth= iTemp;
+
+    fwrite(tbmpfh,sizeof(bmpheader),1,neu);       //Header einlesen
+    fwrite(tbmpif,sizeof(bmpinfo),1,neu);
+
+    for(iY=tbmpif->biHeight ; 0<=iY; iY--)            //Farbtabelle in neue Datei schreiben !In Verbindung mit flipSize! (also iX und iY getauscht)
     {
-        tcolortab[iL] = (bmpcolor*)malloc(width*sizeof(bmpcolor));
+       for(iX=0; iX<tbmpif->biWidth; iX++)
+        {
+            fwrite(&tcolortab[iX][iY],sizeof(bmpcolor),1,neu);
+        }
+
+        if(tbmpif->biWidth%4 != 0)                 // restlichen Bits mit 0 auffüllen
+        {
+            for(iL=0; iL<4-tbmpif->biWidth%4; iL++)
+            {
+                fputc(0,neu);
+            }
+        }
     }
-}*/
+}
 
 
 int main(int argc, char *argv[])
 {
-    int iX , iY, iL, iLZ;
+    int iL, iLZ,iE;
 
-    char neustring[] = "neu.bmp";
+    char neustring[] = "grauneu.bmp";
+    char neustringfunk2[] ="drehneu.bmp";
+    char neustringfunk3[] = "spiegelneu.bmp";
     char fakestring[20];
     char cseek[] = ".";
 
@@ -138,54 +158,118 @@ int main(int argc, char *argv[])
     FILE *neu;
 
 
-    for(iLZ=1; iLZ<=3; iLZ++)
+    printf("Waehlen Sie die Funktion aus:\n");
+    printf("1: Greyscale\n2:Drehen\n3:Spiegeln\n");
+
+    do
     {
+        printf("Geben sie eine Zahl zwischen 1 und 3 ein:\n");
+        scanf("%d",&iE);
+    }while(iE>=4);
 
 
-        printf("::::::::::::::::::::::::%d::::::::::::::::",iLZ);
-        fBmpdatei = fopen(argv[iLZ],"rb"); // Alte Datei öffnen
+    switch(iE)
+    {
+        case 1:
+            for(iLZ=1; iLZ<=3; iLZ++)
+            {
+                printf("::::::::::::::::::::::::%d::::::::::::::::",iLZ);
+                fBmpdatei = fopen(argv[iLZ],"rb"); // Alte Datei öffnen
 
-        printf("%s \n",argv[iLZ]);
-        strcpy(fakestring,argv[iLZ]);
-        strtok(fakestring,cseek); // Zeichen suchen und ab dem Zeichen zerteilen
-        strcat(fakestring,neustring); // Zeichen ersetzen
-        printf("%s \n",fakestring);
+                printf("%s \n",argv[iLZ]);
+                strcpy(fakestring,argv[iLZ]);
+                strtok(fakestring,cseek); // Zeichen suchen und ab dem Zeichen zerteilen
+                strcat(fakestring,neustring); // Zeichen ersetzen
+                printf("%s \n",fakestring);
 
-        neu = fopen(fakestring,"wb"); // neue Datei erstellen
+                neu = fopen(fakestring,"wb"); // neue Datei erstellen
 
-        if(fBmpdatei == NULL) // Geöffnete Datei vorhanden?
-        {
-            printf("Keine Datei wurde geoeffnet.");
-            return 0;
-        }
+                if(fBmpdatei == NULL) // Geöffnete Datei vorhanden?
+                {
+                    printf("Keine Datei wurde geoeffnet.");
+                    return 0;
+                }
 
-        einlesen(fBmpdatei,&bhead,&binfo);
+                einlesen(fBmpdatei,&bhead,&binfo);
 
-        tcolortab = (bmpcolor**)malloc(binfo.biHeight*sizeof(bmpcolor*)); // Speicher reservieren für die erste Dimension
+                tcolortab = (bmpcolor**)malloc(binfo.biHeight*sizeof(bmpcolor*)); // Speicher reservieren für die erste Dimension
 
-        for(iL=0; iL<binfo.biHeight;iL++) // Speicher reservieren für die zweite Dimension
-        {
-            tcolortab[iL] = (bmpcolor*)malloc(binfo.biWidth*sizeof(bmpcolor));
-        }
+                for(iL=0; iL<binfo.biHeight;iL++) // Speicher reservieren für die zweite Dimension
+                {
+                    tcolortab[iL] = (bmpcolor*)malloc(binfo.biWidth*sizeof(bmpcolor));
+                }
 
-        //speicher(binfo.biHeight,binfo.biWidth,tcolortab);
+                tabellealt(neu,binfo.biHeight,binfo.biWidth,tcolortab,fBmpdatei);
 
-        tabellealt(neu,binfo.biHeight, binfo.biWidth, tcolortab, fBmpdatei);
+                greyscale(tcolortab,binfo.biHeight,binfo.biWidth);
 
-        greyscale(tcolortab,binfo.biHeight,binfo.biWidth);
+                auslesen(neu,&bhead,&binfo);
 
-        auslesen(neu,&bhead,&binfo);
+                tabelleneu(neu,binfo.biHeight,binfo.biWidth,tcolortab);
 
-        tabelleneu(neu,binfo.biHeight, binfo.biWidth, tcolortab);
+                printheadinfo(&bhead,&binfo); //Bitmapheader und Bitmapinfo auf der Konsole anzeigen lassen
 
+                fclose(neu); // Neue Datei schließen
+                fclose(fBmpdatei); // Alte Datei schließen
 
-        printheadinfo(&bhead,&binfo); //Bitmapheader und Bitmapinfo auf der Konsole anzeigen lassen
+                printf("%s \n",argv[iLZ]);
+                printf("%s \n",fakestring);
 
-        fclose(neu); // Neue Datei schließen
-        fclose(fBmpdatei); // Alte Datei schließen
+                free(tcolortab);
+                printf("---------------\n");
+            }
+        break;
+        case 2:
+            for(iLZ=1; iLZ<=3; iLZ++)
+            {
+                printf("::::::::::::::::::::::::%d::::::::::::::::",iLZ);
+                fBmpdatei = fopen(argv[iLZ],"rb"); // Alte Datei öffnen
 
-        free(tcolortab);
-        printf("---------------\n");
+                printf("%s \n",argv[iLZ]);
+                strcpy(fakestring,argv[iLZ]);
+                strtok(fakestring,cseek); // Zeichen suchen und ab dem Zeichen zerteilen
+                strcat(fakestring,neustringfunk2); // Zeichen ersetzen
+                printf("%s \n",fakestring);
+                printf("::::::::::::::::::::::::Test::::::::::::::::");
+                neu = fopen(fakestring,"wb"); // neue Datei erstellen
+
+                if(fBmpdatei == NULL) // Geöffnete Datei vorhanden?
+                {
+                    printf("Keine Datei wurde geoeffnet.");
+                    return 0;
+                }
+
+                einlesen(fBmpdatei,&bhead,&binfo);
+
+                tcolortab = (bmpcolor**)malloc(binfo.biHeight*sizeof(bmpcolor*)); // Speicher reservieren für die erste Dimension
+
+                for(iL=0; iL<binfo.biHeight;iL++) // Speicher reservieren für die zweite Dimension
+                {
+                    tcolortab[iL] = (bmpcolor*)malloc(binfo.biWidth*sizeof(bmpcolor));
+                }
+
+                tabellealt(neu,binfo.biHeight,binfo.biWidth,tcolortab,fBmpdatei);
+
+                writeColortabT(neu,&bhead,&binfo,tcolortab);
+
+                auslesen(neu,&bhead,&binfo);
+
+                printheadinfo(&bhead,&binfo); //Bitmapheader und Bitmapinfo auf der Konsole anzeigen lassen
+
+                fclose(neu); // Neue Datei schließen
+                fclose(fBmpdatei); // Alte Datei schließen
+
+                printf("%s \n",argv[iLZ]);
+                printf("%s \n",fakestring);
+
+                free(tcolortab);
+
+                printf("---------------\n");
+            }
+            break;
+        case 3:
+            printf("Funktion 3\n");
+            break;
     }
     return 0;
 }
